f96c13f344853badc2f274022676bf994dbd0c5b
al3x-13 <alexandreromba@hotmail.com>
2023-11-03 03:54:32 +0000

    updates backend main

--- backend/src/main/java/cd/project/backend/Main.java
+++ backend/src/main/java/cd/project/backend/Main.java
@@ -1,8 +1,8 @@
 package cd.project.backend;
 
 import cd.project.backend.database.DbConnection;
-import cd.project.backend.services.ReservationService;
-import cd.project.backend.services.ReservationServiceInterface;
+import cd.project.backend.services.BookingService;
+import cd.project.backend.services.BookingServiceInterface;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -19,19 +19,19 @@ public class Main {
             int port = Integer.parseInt(args[0]);
 
             // services
-            ReservationServiceInterface reservationService = new ReservationService();
+            BookingServiceInterface reservationService = new BookingService();
 
             // stubs
-            UnicastRemoteObject.unexportObject((ReservationServiceInterface) reservationService, true);
-            ReservationServiceInterface reservationServiceStub = (ReservationServiceInterface)
-                    UnicastRemoteObject.exportObject((ReservationServiceInterface) reservationService, 0);
+            UnicastRemoteObject.unexportObject((BookingServiceInterface) reservationService, true);
+            BookingServiceInterface reservationServiceStub = (BookingServiceInterface)
+                    UnicastRemoteObject.exportObject((BookingServiceInterface) reservationService, 0);
 
             // local registry
             Registry registry = LocateRegistry.createRegistry(port);
             registry.bind("ReservationService", reservationServiceStub);
 
             // Initializes DB connection
-            DbConnection.initialize(args[1]);
+            DbConnection db = new DbConnection(args[1]);
 
             System.out.println("RMI server is running");
         } catch (Exception e) {
