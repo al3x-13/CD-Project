90bf0de90912f07004718ee009536821deb13e64
al3x-13 <alexandreromba@hotmail.com>
2023-11-26 10:27:06 +0000

    removes rmi module and moves interfaces to backend

---
+++ server/backend/src/main/java/cd/project/backend/interfaces/BookingServiceInterface.java
@@ -0,0 +1,17 @@
+package cd.project.rmi.interfaces;
+
+import cd.project.backend.domain.Lounge;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public interface BookingServiceInterface extends Remote {
+    String test(String data) throws RemoteException;
+
+   ArrayList<Lounge> listAvailableLounges(
+           char beachId, LocalDate date, LocalTime fromTime, LocalTime toTime
+   ) throws RemoteException;
+}
--- server/rmi/src/main/java/cd/project/rmi/interfaces/BookingServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package cd.project.rmi.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BookingServiceInterface extends Remote {
-    public String test(String data) throws RemoteException;
-}
