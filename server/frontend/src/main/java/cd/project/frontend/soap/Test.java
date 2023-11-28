0788e1329721bfb5beb4f89135ad61d816fcf207
al3x-13 <alexandreromba@hotmail.com>
2023-11-28 00:13:58 +0000

    frontend: soap client testing

--- server/frontend/src/main/java/cd/project/frontend/soap/Test.java
+++ server/frontend/src/main/java/cd/project/frontend/soap/Test.java
@@ -6,5 +6,5 @@ import jakarta.jws.WebService;
 @WebService
 public interface Test {
     @WebMethod
-    int test();
+    String test(int id);
 }
--- server/frontend/src/main/java/cd/project/frontend/soap/TestImpl.java
+++ server/frontend/src/main/java/cd/project/frontend/soap/TestImpl.java
@@ -1,11 +1,22 @@
 package cd.project.frontend.soap;
 
+import cd.project.frontend.database.DbConnection;
 import jakarta.jws.WebService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebService(endpointInterface = "cd.project.frontend.soap.Test")
 public class TestImpl implements Test {
     @Override
-    public int test() {
-        return 69;
+    public String test(int id) {
+        ResultSet data = DbConnection.executeQuery("SELECT username FROM users WHERE id = ?", id);
+        try {
+            if (data == null) return "Not found";
+            data.next();
+            return data.getString("username");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
