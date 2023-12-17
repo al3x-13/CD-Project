46a3000e0dc67aaca4729ac71683ae76a126fc72
al3x-13 <alexandreromba@hotmail.com>
2023-12-17 06:22:37 +0000

    test: more tests

--- server/backend/src/test/java/cd/project/backend/services/BookingServiceHelpersTest.java
+++ server/backend/src/test/java/cd/project/backend/services/BookingServiceHelpersTest.java
@@ -1,11 +1,14 @@
 package cd.project.backend.services;
 
+import cd.project.backend.domain.Lounge;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,6 +22,8 @@ class BookingServiceHelpersTest {
                 LocalTime.of(10, 0),
                 LocalTime.of(11, 0)
         );
+        assertNotNull(realResult);
+        Collections.sort(realResult);
         assertEquals(expectedResult, realResult);
 
         expectedResult = new ArrayList<>( Arrays.asList(8, 9, 10, 11) );
@@ -27,6 +32,8 @@ class BookingServiceHelpersTest {
                 LocalTime.of(10, 0),
                 LocalTime.of(11, 0)
         );
+        assertNotNull(realResult);
+        Collections.sort(realResult);
         assertEquals(expectedResult, realResult);
 
         expectedResult = new ArrayList<>( Arrays.asList(12, 13, 14) );
@@ -35,6 +42,8 @@ class BookingServiceHelpersTest {
                 LocalTime.of(11, 0),
                 LocalTime.of(14, 0)
         );
+        assertNotNull(realResult);
+        Collections.sort(realResult);
         assertEquals(expectedResult, realResult);
     }
 
@@ -49,30 +58,114 @@ class BookingServiceHelpersTest {
     }
 
     @Test
-    void getAvailableLounges() {
-        ArrayList<String> expectedReesult = new ArrayList<>(Arrays.asList(
-                "",
-                ""
+    void getAvailableLounges_LoungesLeft() {
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList(
+                "A6","A7","A8","A9","A10","A14","A15","A19","A20"
+        ));
+        ArrayList<String> realResult = new ArrayList<>(
+                BookingServiceHelpers.getAvailableLounges(
+                    'A',
+                    LocalDate.of(2024, 6, 12),
+                    LocalTime.of(10, 0),
+                    LocalTime.of(11, 0)
+                ).stream().map(Lounge::getId).collect(Collectors.toList())
+        );
+        boolean containsAllValues = true;
+        for (String id : realResult) {
+            if (!expectedResult.contains(id)) {
+                containsAllValues = false;
+                break;
+            }
+        }
+        assertTrue(containsAllValues);
+
+        expectedResult = new ArrayList<>(Arrays.asList(
+                "B4","B5","B9","B10"
+        ));
+        realResult = new ArrayList<>(
+                BookingServiceHelpers.getAvailableLounges(
+                        'B',
+                        LocalDate.of(2024, 6, 13),
+                        LocalTime.of(10, 0),
+                        LocalTime.of(11, 0)
+                ).stream().map(Lounge::getId).collect(Collectors.toList())
+        );
+        for (String id : realResult) {
+            if (!expectedResult.contains(id)) {
+                containsAllValues = false;
+                break;
+            }
+        }
+        assertTrue(containsAllValues);
+    }
+
+    @Test
+    void getAvailableLounges_NoLoungesLeft() {
+        assertNull(BookingServiceHelpers.getAvailableLounges(
+                'C',
+                LocalDate.of(2024, 6, 14),
+                LocalTime.of(11, 0),
+                LocalTime.of(14, 0)
         ));
     }
 
     @Test
-    void getBookingIdsForDate() {
+    void createBooking_ValidBookings() {
+        assertTrue(BookingServiceHelpers.createBooking(
+                'A',
+                LocalDate.of(2024, 6, 18),
+                LocalTime.of(10, 0),
+                LocalTime.of(11, 0),
+                5,
+                1
+        ));
+
+        assertTrue(BookingServiceHelpers.createBooking(
+                'B',
+                LocalDate.of(2024, 6, 19),
+                LocalTime.of(10, 0),
+                LocalTime.of(11, 0),
+                8,
+                1
+        ));
+
+        assertTrue(BookingServiceHelpers.createBooking(
+                'A',
+                LocalDate.of(2024, 6, 18),
+                LocalTime.of(14, 0),
+                LocalTime.of(18,0),
+                14,
+                1
+        ));
     }
 
     @Test
-    void testGetBookingIdsForDateAndTime() {
-    }
+    void createBooking_InvalidBookings() {
+        assertFalse(BookingServiceHelpers.createBooking(
+                'A',
+                LocalDate.of(2024, 6, 12),
+                LocalTime.of(10, 0),
+                LocalTime.of(11, 0),
+                30,
+                1
+        ));
 
-    @Test
-    void getRemainingLoungeSeats() {
-    }
+        assertFalse(BookingServiceHelpers.createBooking(
+                'C',
+                LocalDate.of(2024, 6, 14),
+                LocalTime.of(11, 0),
+                LocalTime.of(14, 0),
+                5,
+                1
+        ));
 
-    @Test
-    void getTotalLoungeSeats() {
-    }
-
-    @Test
-    void createBooking() {
+        assertFalse(BookingServiceHelpers.createBooking(
+                'C',
+                LocalDate.of(2024, 6, 14),
+                LocalTime.of(11, 0),
+                LocalTime.of(12, 0),
+                1,
+                1
+        ));
     }
 }
