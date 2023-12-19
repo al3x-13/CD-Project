78110b37d8630e5f1b640a214ef2a88bae3e2c17
al3x-13 <alexandreromba@hotmail.com>
2023-12-19 14:11:45 +0000

    feat: improves error handling in booking service client

--- server/frontend/src/main/java/cd/project/frontend/soap/client/rmi/BookingServiceClient.java
+++ server/frontend/src/main/java/cd/project/frontend/soap/client/rmi/BookingServiceClient.java
@@ -3,27 +3,36 @@ package cd.project.frontend.soap.client.rmi;
 import cd.project.backend.interfaces.BookingServiceInterface;
 import cd.project.backend.services.BookingService;
 
+import java.rmi.AlreadyBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class BookingServiceClient {
-    public BookingServiceInterface BookingServiceClient() {
-        BookingServiceInterface bookingService;
+    private BookingServiceInterface bookingService;
+
+    public BookingServiceClient() {
         try {
-            bookingService = new BookingService();
+            this.bookingService = new BookingService();
 
             // create stub
-            UnicastRemoteObject.unexportObject((BookingServiceInterface) bookingService, true);
+            UnicastRemoteObject.unexportObject((BookingServiceInterface) this.bookingService, true);
             BookingServiceInterface bookingServiceStub = (BookingServiceInterface)
-                    UnicastRemoteObject.exportObject((BookingServiceInterface) bookingService, 0);
+                    UnicastRemoteObject.exportObject((BookingServiceInterface) this.bookingService, 0);
 
             // registry setup (local)
-            Registry registry = LocateRegistry.createRegistry(Integer.parseInt(System.getenv("port")));
-            registry.bind("BookingService", bookingServiceStub);
+            Registry registry = LocateRegistry.getRegistry();
+            try {
+                registry.bind("BookingService", bookingServiceStub);
+            } catch (AlreadyBoundException e) {
+                registry.rebind("BookingService", bookingServiceStub);
+            }
         } catch (Exception e) {
             throw new RuntimeException("Failed to instantiate booking service: " + e);
         }
-        return bookingService;
+    }
+
+    public BookingServiceInterface getBookingService() {
+        return this.bookingService;
     }
 }
