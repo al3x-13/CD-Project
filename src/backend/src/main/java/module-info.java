605f4bfca09311b401daad6910e2a2a4c99b244c
al3x-13 <alexandreromba@hotmail.com>
2023-12-27 23:07:03 +0000

    chore: backend module-info update

--- src/backend/src/main/java/module-info.java
+++ src/backend/src/main/java/module-info.java
@@ -1,4 +1,7 @@
 module cd.project.backend {
+    requires java.sql;
+    requires jakarta.xml.bind;
+    requires java.rmi;
     exports cd.project.backend.interfaces;
     exports cd.project.backend.services;
 }
