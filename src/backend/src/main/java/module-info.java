6b8323cca7fdc120e7a41734593010d0a54e38bc
al3x-13 <alexandreromba@hotmail.com>
2023-12-28 03:33:13 +0000

    chore: f*ck module-info files

--- src/backend/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cd.project.backend {
-    requires java.sql;
-    requires java.rmi;
-    requires jakarta.xml.bind;
-    exports cd.project.backend.interfaces;
-    exports cd.project.backend.services;
-}
