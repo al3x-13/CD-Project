beb069f56589260ae4d2de0b9c493b02ee70a778
al3x-13 <alexandreromba@hotmail.com>
2024-01-01 16:48:40 +0000

    feat: logging tweak (tomcat)

--- src/frontend/src/main/java/cd/project/frontend/auth/AuthMiddleware.java
+++ src/frontend/src/main/java/cd/project/frontend/auth/AuthMiddleware.java
@@ -11,14 +11,9 @@ import java.io.IOException;
 public class AuthMiddleware extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // TODO: remove this
-        System.out.println(request.getMethod() + " at " + request.getRequestURI());
-        System.out.println("Auth header: " + request.getHeader("Authorization"));
         String uri = request.getRequestURI();
 
         if (AuthenticationHelpers.endpointIsProtected(uri)) {
-            System.out.println("PROTECTED ENDPOINT");
-
             String authHeader = request.getHeader("Authorization");
             if (authHeader == null) {
                 response.sendError(401, "Missing 'Authorization' header");
@@ -36,7 +31,10 @@ public class AuthMiddleware extends OncePerRequestFilter {
                 response.sendError(401, "JWT is not valid");
                 return;
             }
-            System.out.println("REQUEST by '" + jwt.getSubject() + "'");
+            System.out.println("(PROTECTED) " + request.getMethod() + " at " + request.getRequestURI() + " by '" +
+            jwt.getSubject() + "' (id: " + JwtHelper.getUserId(token) + ")");
+        } else {
+            System.out.println(request.getMethod() + " at " + request.getRequestURI());
         }
         filterChain.doFilter(request, response);
     }
